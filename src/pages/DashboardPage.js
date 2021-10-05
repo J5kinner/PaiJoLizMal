d1d3b3fbdb5728ec52bb039f66e7a419be66032c
Jonah Skinner <jonah.lee.skinner@gmail.com>
2021-10-05 12:26:59 +1100

    refactored files and added front end pages

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,23 @@
+/** @license 3120 Group X
+  * Copyright (c) 3120 Group X, Inc. and its affiliates.
+  *
+  * Authors:
+  * 
+  */
+
+import React from 'react'
+
+function DashboardPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+DashboardPage.propTypes = {
+
+}
+
+export default DashboardPage
+
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,18 @@
+/** @license 3120 Group X
+  * Copyright (c) 3120 Group X, Inc. and its affiliates.
+  *
+  * Authors: 
+  * 
+  */
+
+import React from 'react'
+
+function SignUpPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SignUpPage
