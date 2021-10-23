84526ef289689bc6e1c1e80fb98b04cc4b28c0e9
paigelea <paigeleaanthony@gmail.com>
2021-10-23 17:48:37 +1100

    implement basic home page grid styling

--- src/components/Note.js
+++ src/components/Note.js
@@ -9,31 +9,33 @@ import React from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+
+import Colours from "../assets/Colours";
 import '../assets/scss/components/Shop.scss'
 
 
-const Note = ({note}) => {
-  console.log("hello", note)
-  return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <div className={"shop-item shop-item-" + note.background}>
-        <Typography variant="h5" >
-          {note.title}
-        </Typography>
+const Note = ({ note }) => {
+    console.log("hello", note)
 
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {note.username}
-        </Typography>
-        <Typography variant="body2">
-          {note.body}
-          <br />
-        </Typography>
-        </div>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card sx={{ height: 200, boxShadow: 0, background: Colours.gray }}>
+            <CardContent>
+                <div className={"shop-item shop-item-" + note.background}>
+                    <Typography variant="h5" >
+                        {note.title}
+                    </Typography>
+
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {note.username}
+                    </Typography>
+                    <Typography variant="body2">
+                        {note.body}
+                        <br />
+                    </Typography>
+                </div>
+            </CardContent>
+        </Card>
+    )
 }
 
-export default Note;
-
+export default Note
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -5,40 +5,56 @@
  *
  */
 
-import { React, useState, useEffect } from "react";
-import Note from "../components/Note";
+import { React, useState, useEffect } from "react"
+
+import Box  from "@mui/material/Box"
+import Typography  from "@mui/material/Typography"
+
+import Colours from '../assets/Colours'
+
 import "../assets/scss/layout/App.scss"
-import NoteService from "../services/NoteService";
+import NoteService from "../services/NoteService"
+import NotesGrid from "../components/NotesGrid"
 
 function HomePage() {
   const [notes, setNotes] = useState([])
   
-  useEffect(() => {
-    NoteService.getAllNotes()
-    .then(notes => {
-      if (notes) {
-        setNotes(notes)
-      } else {
-      setNotes({
-        title: "Title",
-        body: "Body",
-        username: "Username",
-        background: ""
-      })
-    }
-  })
-  },[])
-  console.log(notes)
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <div className="note-space">
-      {notes.map((note) => 
-          <Note note={note}/>
-      )}
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        NoteService.getAllNotes()
+        .then(notes => {
+            if (notes) {
+                setNotes(notes)
+            } else {
+                setNotes({
+                    title: "Title",
+                    body: "Body",
+                    username: "Username",
+                    background: ""
+                })
+            }
+        })
+    },[])
+
+    return (
+        <div>
+            <Box sx={{ display: 'flex' }}>
+                <Typography variant='title'>Rants</Typography>
+                <Box sx={{ flexGrow: 1 }} />
+                <Typography variant='title'
+                    sx={{ ml: 5,
+                        '&:hover': { color: Colours.carrot, },
+                        '&:active': { color: Colours.rust, },
+                    }}
+                >
+                    Filter
+                </Typography>
+            </Box>
+
+            <Box sx={{ margin: { xs: 0, md: 5} }}>
+                <NotesGrid notes={notes} />
+            </Box>
+        </div>
+    );
 }
 
 export default HomePage;
