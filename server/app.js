537fc811429d844140dbd54f46dc0b82cb37ee9b
elizabeththerese <elizabeth.laham@gmail.com>
2021-11-03 22:42:41 +1100

    amend joined paths

--- server/app.js
+++ server/app.js
@@ -3,12 +3,16 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const apiRouter = require('./controllers/api')
-
+const path = require('path')
 const app = express() 
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(__dirname +'/build'));
+app.use(express.static('build'));
 app.use(apiRouter)
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 module.exports = app
